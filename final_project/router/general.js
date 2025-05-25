ed22243fdf9b50351de6fa320e1ff5376cf2a397
hzmdrl <hzmdrl@gmail.com>
2025-05-25 04:33:30 -0400

    complete tasks 1 to 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,26 +18,59 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const matchedBooks = [];
+
+    const keys = Object.keys(books);
+
+    for (const key of keys) {
+        const book = books[key];
+        if (book.author === author) {
+            matchedBooks.push(book);
+        }
+    }
+
+    if (matchedBooks.length > 0) {
+        res.send(JSON.stringify(matchedBooks,null,4));
+    } else {
+        res.status(404).send("No books found by that author");
+    }
+
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const title = req.params.title;
+    const matchedBooks = [];
+
+    const keys = Object.keys(books);
+
+    for (const key of keys) {
+        const book = books[key];
+        if (book.title === title) {
+            matchedBooks.push(book);
+        }
+    }
+
+    if (matchedBooks.length > 0) {
+        res.send(JSON.stringify(matchedBooks,null,4));
+    } else {
+        res.status(404).send("No books found by that author");
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
