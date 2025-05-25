e72677a58fa61e82dd8bd806ec10a11777d304af
hzmdrl <hzmdrl@gmail.com>
2025-05-24 20:20:39 -0400

    modified general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // send JSON response with formatted book data
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
